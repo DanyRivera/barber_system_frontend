7fbf43e88a064adccd763fe01fd633a529096c50
Dany <dany@gmail.com>
2026-04-08 20:01:47 -0600

    Protect public views

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,7 +1,22 @@
 import { Outlet } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { Navigate } from "react-router-dom";
+
+import { getUser } from "../api";
+import LoadingView from "../views/LoadingView";
+
 
 const PublicLayout = () => {
 
+    const { isLoading, isError } = useQuery({
+        queryKey: ['user'],
+        queryFn: getUser,
+        retry: 1
+    })
+
+
+    if (isLoading) return <LoadingView />
+    if (!isError) return <Navigate to={'/admin/citas'} />
     return (
         <div className="min-h-screen bg-[#0d0d0d] flex flex-col items-center justify-center relative overflow-hidden">
             <Outlet />
